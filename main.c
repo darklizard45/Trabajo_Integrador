bfc5c29385256da7b7198b02ca8a58eaf4ccd593
darklizard45 <ramirolisandrokovalski@hotmail.com>
2020-11-15 22:07:22 -0300

    Add files via upload

--- main.c
+++ main.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include "usuario.h"
+#include "recargas.h"
+#include "inspecion.h"
 
 /* Menu y rama principal del programa */
 void cabecera();
@@ -9,7 +12,7 @@ void main() {
 	int op,ban=1;
 	while(ban){
 		cabecera();
-		printf("\n\t\t1-LOGIN\n\t\t2-REGISTRAR\n\t\t3-INSPECTOR\n\t\t4-CERRAR\n");
+		printf("\n\t\t1-LOGIN\n\t\t2-REGISTRAR\n\t\t3-INSPECTOR\n\t\t4-CERRAR\n\t\t5.MOSTRAR USUARIOS\n\t\t6.MOSTRAR SALDOS DE USUARIO\n\t\t7.MOSTRAR MOVIMIENTOS");
 		scanf("%d",&op);
 		getchar();
 		switch(op){
@@ -20,10 +23,19 @@ void main() {
 				registers();
 				break;
 			case 3:
-				opcionesIns();
+				inspector();
 				break;
 			case 4:
 				ban=0;
+				break;
+			case 5:
+				mostraruser();
+				break;
+			case 6:
+				user_saldos();
+				break;
+			case 7:
+				mostrarmovimientos();
 				break;
 			default:
 				printf("No es una de las opciones\n");
